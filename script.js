5c755e0ab33dbefdb66265493bb3e71ae56cc502
Sayra Flores <sayraflores@MacBook-Pro-2.local>
2022-04-22 13:58:44 -0400

    another 1

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-const nameofStudents['sarah', 'brandon', 'tiffany', 'jake', adam'];
-console.log(nameofStudents);
+let string =['sarah', 'brandon', 'tiffany', 'jake', 'adam'];
+let numbers= [5,3,2,10,6];
+let boolean=[10>9, 7>6, 6>5, 4>3, 2>1];
+
+function methods(strings,numbers,boolean) {
+const namesInPod = (strings[0], strings[1], strings[2], strings[3], strings[4]);
